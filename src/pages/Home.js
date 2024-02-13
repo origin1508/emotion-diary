2c896f735d89c7eaf903bd965a55aa986f85cd7c
origin1508 <origin1508@gmail.com>
2024-02-13 23:50:29 +0900

    feat: Home 헤더 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    setCurDate(
+      (prev) =>
+        new Date(prev.getFullYear(), prev.getMonth() + 1, prev.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      (prev) =>
+        new Date(prev.getFullYear(), prev.getMonth() - 1, prev.getDate())
+    );
+  };
+
+  console.log(curDate);
+
   return (
     <div>
-      <h1>Home.js</h1>
-      <p>This is Home Page</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      ></MyHeader>
     </div>
   );
 };
