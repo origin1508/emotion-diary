4cb80ad1ef2cc3c6c6a148de0d8f9d847a2b2bbd
origin1508 <origin1508@gmail.com>
2024-02-22 19:30:05 +0900

    feat: 일기 수정 기능 구현

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,30 @@
+import { useState, useContext, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
+
 const Edit = () => {
+  const [originData, setOriginData] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
   return (
     <div>
-      <h1>Edit.js</h1>
-      <p>This is Edit Page</p>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
